044a957b7149ee872cd8f8ebe9db2357f43e642a
Roibruler <adamroy7428@gmail.com>
2026-02-18 17:08:37 -0500

    Users presque fini V2

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post} from '@nestjs/common';
 import { UserService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller()
+@Controller('users')
 export class UserController {
     
     constructor(private service: UserService){}
 
     @Post()
-    createUser(@Body() body : any){
+    createUser(@Body() body : CreateUserDto){
         console.log(body)
     }
 }
